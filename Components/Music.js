363dba50bfc5a2db22300d8ce130042b90fa3e81
Jana <67344963+iitsjana@users.noreply.github.com>
2022-04-29 13:15:50 +0300

    Add files via upload

---
+++ Components/Music.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import NotesApp from "./Notes/NotesApp";
+
+const Music = () => {
+    return (
+
+        <section id="notes">
+            <div>
+                <NotesApp/>
+            </div>
+        </section>
+
+    );
+};
+
+export default Music;
+
